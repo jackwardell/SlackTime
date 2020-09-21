00761db296fb3a39a12ae27575c13a6f4c16e677
jackwardell <jackwardell@me.com>
2020-09-21 16:09:17 +0100

    manual check: apps.py

--- slack_time/methods/apps.py
+++ slack_time/methods/apps.py
@@ -1,7 +1,211 @@
 # -*- coding: utf-8 -*-
 from requests import Response
+
 from slack_time.api import SlackAPI
 from slack_time.utils import cached_property
+
+
+class Resources(SlackAPI):
+    def list(self, cursor: str = None, limit: int = None, **kwargs) -> Response:
+        """
+        Returns list of resource grants this app has on a team.
+        https://api.slack.com/methods/apps.permissions.resources.list
+
+        :param token: Authentication token bearing required scopes.
+        :type str: e.g. xxxx-xxxxxxxxx-xxxx
+
+        :param cursor: Paginate through collections of data by setting the cursor parameter to a next_cursor attribute returned by a previous request's response_metadata. Default value fetches the first "page" of the collection. See pagination for more detail.
+        :type str: e.g. dXNlcjpVMDYxTkZUVDI=
+
+        :param limit: The maximum number of items to return.
+        :type int: e.g. 20
+
+        :returns response:
+        :type requests.Response: e.g. <Response [200]>
+
+        example:
+        >>> client = SlackTime(token='insert-your-token-here')
+        >>> response = client.apps.permissions.resources.list(**your_params)
+        <Response [200]>
+        >>> response.json()
+        {
+            "ok": true,
+            "resources": [
+                {
+                    "id": "T0DES3UAN",
+                    "type": "team"
+                },
+                {
+                    "id": "D024BFF1M",
+                    "type": "app_home"
+                },
+                {
+                    "id": "C024BE91L",
+                    "type": "channel"
+                }
+            ],
+            "response_metadata": {
+                "next_cursor": "dGVhbTpDMUg5UkVTR0w="
+            }
+        }
+        """
+
+        payload = {"token": self.token}
+
+        if cursor is not None:
+            payload["cursor"] = cursor
+
+        if limit is not None:
+            payload["limit"] = limit
+
+        return self._get("apps.permissions.resources.list", payload=payload, **kwargs)
+
+
+class Scopes(SlackAPI):
+    def list(self, **kwargs) -> Response:
+        """
+        Returns list of scopes this app has on a team.
+        https://api.slack.com/methods/apps.permissions.scopes.list
+
+        :param token: Authentication token bearing required scopes.
+        :type str: e.g. xxxx-xxxxxxxxx-xxxx
+
+        :returns response:
+        :type requests.Response: e.g. <Response [200]>
+
+        example:
+        >>> client = SlackTime(token='insert-your-token-here')
+        >>> response = client.apps.permissions.scopes.list(**your_params)
+        <Response [200]>
+        >>> response.json()
+        {
+            "ok": true,
+            "scopes": {
+                "app_home": [
+                    "chat:write",
+                    "im:history",
+                    "im:read"
+                ],
+                "team": [
+                    "users:read"
+                ],
+                "channel": [
+                    "channels:history",
+                    "chat:write"
+                ],
+                "group": [
+                    "chat:write"
+                ],
+                "mpim": [
+                    "chat:write"
+                ],
+                "im": [
+                    "chat:write"
+                ],
+                "user": []
+            }
+        }
+        """
+
+        payload = {"token": self.token}
+
+        return self._get("apps.permissions.scopes.list", payload=payload, **kwargs)
+
+
+class Users(SlackAPI):
+    def list(self, cursor: str = None, limit: int = None, **kwargs) -> Response:
+        """
+        Returns list of user grants and corresponding scopes this app has on a team.
+        https://api.slack.com/methods/apps.permissions.users.list
+
+        :param token: Authentication token bearing required scopes.
+        :type str: e.g. xxxx-xxxxxxxxx-xxxx
+
+        :param cursor: Paginate through collections of data by setting the cursor parameter to a next_cursor attribute returned by a previous request's response_metadata. Default value fetches the first "page" of the collection. See pagination for more detail.
+        :type str: e.g. dXNlcjpVMDYxTkZUVDI=
+
+        :param limit: The maximum number of items to return.
+        :type int: e.g. 20
+
+        :returns response:
+        :type requests.Response: e.g. <Response [200]>
+
+        example:
+        >>> client = SlackTime(token='insert-your-token-here')
+        >>> response = client.apps.permissions.users.list(**your_params)
+        <Response [200]>
+        >>> response.json()
+        {
+            "ok": true,
+            "resources": [
+                {
+                    "id": "U0DES3UAN",
+                    "scopes": [
+                        "dnd:write:user",
+                        "reminders:write:user"
+                    ]
+                },
+                {
+                    "id": "U024BFF1M",
+                    "scopes": [
+                        "reminders:write:user"
+                    ]
+                }
+            ],
+            "response_metadata": {
+                "next_cursor": "dGVhbTdPMUg5UkFTT0w="
+            }
+        }
+        """
+
+        payload = {"token": self.token}
+
+        if cursor is not None:
+            payload["cursor"] = cursor
+
+        if limit is not None:
+            payload["limit"] = limit
+
+        return self._get("apps.permissions.users.list", payload=payload, **kwargs)
+
+    def request(self, scopes: str, trigger_id: str, user: str, **kwargs) -> Response:
+        """
+        Enables an app to trigger a permissions modal to grant an app access to a user access scope.
+        https://api.slack.com/methods/apps.permissions.users.request
+
+        :param token: Authentication token bearing required scopes.
+        :type str: e.g. xxxx-xxxxxxxxx-xxxx
+
+        :param scopes: A comma separated list of user scopes to request for
+        :type str:
+
+        :param trigger_id: Token used to trigger the request
+        :type str:
+
+        :param user: The user this scope is being requested for
+        :type str:
+
+        :returns response:
+        :type requests.Response: e.g. <Response [200]>
+
+        example:
+        >>> client = SlackTime(token='insert-your-token-here')
+        >>> response = client.apps.permissions.users.request(**your_params)
+        <Response [200]>
+        >>> response.json()
+        {
+            "ok": true
+        }
+        """
+
+        payload = {
+            "token": self.token,
+            "scopes": scopes,
+            "trigger_id": trigger_id,
+            "user": user,
+        }
+
+        return self._get("apps.permissions.users.request", payload=payload, **kwargs)
 
 
 class Permissions(SlackAPI):
@@ -12,7 +216,6 @@ class Permissions(SlackAPI):
 
         :param token: Authentication token bearing required scopes.
         :type str: e.g. xxxx-xxxxxxxxx-xxxx
-
 
         :returns response:
         :type requests.Response: e.g. <Response [200]>
@@ -92,6 +295,7 @@ class Permissions(SlackAPI):
 
         :param scopes: A comma separated list of scopes to request for
         :type str:
+
         :param trigger_id: Token used to trigger the permissions API
         :type str:
 
@@ -112,209 +316,17 @@ class Permissions(SlackAPI):
 
         return self._get("apps.permissions.request", payload=payload, **kwargs)
 
+    @cached_property
+    def resources(self) -> Resources:
+        return Resources(**self.params)
 
-class Resources(SlackAPI):
-    def list(self, cursor: str = None, limit: int = None, **kwargs) -> Response:
-        """
-        Returns list of resource grants this app has on a team.
-        https://api.slack.com/methods/apps.permissions.resources.list
+    @cached_property
+    def scopes(self):
+        return Scopes(**self.params)
 
-        :param token: Authentication token bearing required scopes.
-        :type str: e.g. xxxx-xxxxxxxxx-xxxx
-
-        :param cursor: Paginate through collections of data by setting the cursor parameter to a next_cursor attribute returned by a previous request's response_metadata. Default value fetches the first "page" of the collection. See pagination for more detail.
-        :type str: e.g. dXNlcjpVMDYxTkZUVDI=
-
-        :param limit: The maximum number of items to return.
-        :type int: e.g. 20
-
-
-        :returns response:
-        :type requests.Response: e.g. <Response [200]>
-
-        example:
-        >>> client = SlackTime(token='insert-your-token-here')
-        >>> response = client.apps.permissions.resources.list(**your_params)
-        <Response [200]>
-        >>> response.json()
-        {
-            "ok": true,
-            "resources": [
-                {
-                    "id": "T0DES3UAN",
-                    "type": "team"
-                },
-                {
-                    "id": "D024BFF1M",
-                    "type": "app_home"
-                },
-                {
-                    "id": "C024BE91L",
-                    "type": "channel"
-                }
-            ],
-            "response_metadata": {
-                "next_cursor": "dGVhbTpDMUg5UkVTR0w="
-            }
-        }
-        """
-
-        payload = {"token": self.token}
-
-        if cursor is not None:
-            payload["cursor"] = cursor
-
-        if limit is not None:
-            payload["limit"] = limit
-
-        return self._get("apps.permissions.resources.list", payload=payload, **kwargs)
-
-
-class Scopes(SlackAPI):
-    def list(self, **kwargs) -> Response:
-        """
-        Returns list of scopes this app has on a team.
-        https://api.slack.com/methods/apps.permissions.scopes.list
-
-        :param token: Authentication token bearing required scopes.
-        :type str: e.g. xxxx-xxxxxxxxx-xxxx
-
-
-        :returns response:
-        :type requests.Response: e.g. <Response [200]>
-
-        example:
-        >>> client = SlackTime(token='insert-your-token-here')
-        >>> response = client.apps.permissions.scopes.list(**your_params)
-        <Response [200]>
-        >>> response.json()
-        {
-            "ok": true,
-            "scopes": {
-                "app_home": [
-                    "chat:write",
-                    "im:history",
-                    "im:read"
-                ],
-                "team": [
-                    "users:read"
-                ],
-                "channel": [
-                    "channels:history",
-                    "chat:write"
-                ],
-                "group": [
-                    "chat:write"
-                ],
-                "mpim": [
-                    "chat:write"
-                ],
-                "im": [
-                    "chat:write"
-                ],
-                "user": []
-            }
-        }
-        """
-
-        payload = {"token": self.token}
-
-        return self._get("apps.permissions.scopes.list", payload=payload, **kwargs)
-
-
-class Users(SlackAPI):
-    def list(self, cursor: str = None, limit: int = None, **kwargs) -> Response:
-        """
-        Returns list of user grants and corresponding scopes this app has on a team.
-        https://api.slack.com/methods/apps.permissions.users.list
-
-        :param token: Authentication token bearing required scopes.
-        :type str: e.g. xxxx-xxxxxxxxx-xxxx
-
-        :param cursor: Paginate through collections of data by setting the cursor parameter to a next_cursor attribute returned by a previous request's response_metadata. Default value fetches the first "page" of the collection. See pagination for more detail.
-        :type str: e.g. dXNlcjpVMDYxTkZUVDI=
-
-        :param limit: The maximum number of items to return.
-        :type int: e.g. 20
-
-
-        :returns response:
-        :type requests.Response: e.g. <Response [200]>
-
-        example:
-        >>> client = SlackTime(token='insert-your-token-here')
-        >>> response = client.apps.permissions.users.list(**your_params)
-        <Response [200]>
-        >>> response.json()
-        {
-            "ok": true,
-            "resources": [
-                {
-                    "id": "U0DES3UAN",
-                    "scopes": [
-                        "dnd:write:user",
-                        "reminders:write:user"
-                    ]
-                },
-                {
-                    "id": "U024BFF1M",
-                    "scopes": [
-                        "reminders:write:user"
-                    ]
-                }
-            ],
-            "response_metadata": {
-                "next_cursor": "dGVhbTdPMUg5UkFTT0w="
-            }
-        }
-        """
-
-        payload = {"token": self.token}
-
-        if cursor is not None:
-            payload["cursor"] = cursor
-
-        if limit is not None:
-            payload["limit"] = limit
-
-        return self._get("apps.permissions.users.list", payload=payload, **kwargs)
-
-    def request(self, scopes: str, trigger_id: str, user: str, **kwargs) -> Response:
-        """
-        Enables an app to trigger a permissions modal to grant an app access to a user access scope.
-        https://api.slack.com/methods/apps.permissions.users.request
-
-        :param token: Authentication token bearing required scopes.
-        :type str: e.g. xxxx-xxxxxxxxx-xxxx
-
-        :param scopes: A comma separated list of user scopes to request for
-        :type str:
-        :param trigger_id: Token used to trigger the request
-        :type str:
-        :param user: The user this scope is being requested for
-        :type str:
-
-        :returns response:
-        :type requests.Response: e.g. <Response [200]>
-
-        example:
-        >>> client = SlackTime(token='insert-your-token-here')
-        >>> response = client.apps.permissions.users.request(**your_params)
-        <Response [200]>
-        >>> response.json()
-        {
-            "ok": true
-        }
-        """
-
-        payload = {
-            "token": self.token,
-            "scopes": scopes,
-            "trigger_id": trigger_id,
-            "user": user,
-        }
-
-        return self._get("apps.permissions.users.request", payload=payload, **kwargs)
+    @cached_property
+    def users(self) -> Users:
+        return Users(**self.params)
 
 
 class Apps(SlackAPI):
@@ -336,7 +348,6 @@ class Apps(SlackAPI):
         :param client_secret: Issued when you created your application.
         :type str: e.g. f25b5ceaf8a3c2a2c4f52bb4f0b0499e
 
-
         :returns response:
         :type requests.Response: e.g. <Response [200]>
 
@@ -353,13 +364,3 @@ class Apps(SlackAPI):
         }
 
         return self._get("apps.uninstall", payload=payload, **kwargs)
-
-
-class Apps(SlackAPI):
-    @cached_property
-    def permissions(self) -> Permissions:
-        return Permissions(**self.params)
-
-    @cached_property
-    def apps(self) -> Apps:
-        return Apps(**self.params)
