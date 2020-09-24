667e31cc0ece6f82af89e5f9f56829740e46a221
jack <jackwardell@me.com>
2020-09-24 16:09:21 +0100

    Update __init__.py
    changing assertion error to include actual env var chosen

--- slack_time/__init__.py
+++ slack_time/__init__.py
@@ -6,5 +6,5 @@ from slack_time.methods import SlackTime
 
 def get_slack_time(env_var: str = "SLACK_API_TOKEN", **kwargs) -> SlackTime:
     token = os.getenv(env_var)
-    assert token, "You must save a 'SLACK_API_TOKEN' environment variable"
+    assert token, f"You must save a '{env_var}' environment variable"
     return SlackTime(token, **kwargs)
